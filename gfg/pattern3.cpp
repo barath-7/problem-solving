6bc042e5086a56a97e4745de0cea494f14145327
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-11-17 23:19:50 +0530

    gfg pattern-3 solution

---
+++ gfg/pattern3.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int printTriangle(int n);
+int main()
+{
+    int n;
+    cin >> n;
+    
+    printTriangle(n);
+    return 0;
+}
+
+int printTriangle(int n)
+    {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+
+/*
+
+sample output for input 5
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+
+*/
